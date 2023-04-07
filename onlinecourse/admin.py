a71621c553ccb56ef6a8c7bf5a04aa16653c1057
HexVexDev <hexvexdev@gmail.com>
2023-04-07 03:33:07 -0400

    Haciendo cambios

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra= 4
+
+class QuestionInLine(admin.StackedInline):
+    model = Question
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['ctext']
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+    list_display=['qtext']
+
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 5
+    extra = 2
 
 
 # Register your models here.
@@ -28,3 +43,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice,ChoiceAdmin)
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,20 +101,29 @@ class Enrollment(models.Model):
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
+class Question(models.Model):
+    #Many to many relationship with course
+    courses = models.ManyToManyField(Course)
     # Foreign key to lesson
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     # question text
+    qtext = models.CharField(null=False, max_length=200)
     # question grade/mark
+    qvalue = models.IntegerField(null=False)
+
+  
 
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
-
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+    def __str__(self):
+        return self.qtext + "," + \
+               self.lesson.title
 
 #  <HINT> Create a Choice Model with:
     # Used to persist choice content for a question
@@ -122,13 +131,16 @@ class Enrollment(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+ question_id = models.ForeignKey(Question, on_delete=models.CASCADE, default=0)
+ ctext = models.CharField(null=False, max_length=50)
+ iscorrect = models.BooleanField(default=False)
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    chocies = models.ManyToManyField(Choice)
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
